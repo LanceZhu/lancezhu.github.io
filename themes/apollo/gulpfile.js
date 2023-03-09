ef1b656626cdf1932cb8686460a3fc6ea4102143
Lance Zhu <lancezhu2937@gmail.com>
2023-03-09 18:36:18 +0800

    style: upgrade apollo theme

--- themes/apollo/gulpfile.js
+++ themes/apollo/gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var autoprefixer = require('gulp-autoprefixer');
 
-// 一次性编译 Sass 
-gulp.task('sass', function() {
-    return gulp.src('./source/scss/*.scss')
+function build(cb){
+    gulp.src('./source/scss/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
+        // .pipe(sass({outputStyle: 'expanded'}))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
-});
+    cb();
+};
 
-// 实时编译
-gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
-    gulp.watch('./source/scss/*.scss', ['sass']);
-});
+exports.build = build;
+exports.default = function(){
+    gulp.watch('./source/scss/_partial/*.scss', build);
+    gulp.watch('./source/scss/*.scss', build);
+}
+
