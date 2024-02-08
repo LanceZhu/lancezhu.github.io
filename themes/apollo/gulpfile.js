678a08c9e67a7faf85f0a71dd80c4f7fb60fec37
Lance Zhu <lancezhu2937@gmail.com>
2024-02-09 00:19:36 +0800

    style: darkmode, font

--- themes/apollo/gulpfile.js
+++ themes/apollo/gulpfile.js
@@ -1,19 +1,37 @@
+// var gulp = require('gulp');
+// var sass = require('gulp-sass')(require('sass'));
+// var autoprefixer = require('gulp-autoprefixer');
+//
+// function build(cb){
+//     gulp.src('./source/scss/*.scss')
+//         .pipe(sass({outputStyle: 'compressed'}))
+//         // .pipe(sass({outputStyle: 'expanded'}))
+//         .pipe(autoprefixer())
+//         .pipe(gulp.dest('./source/css'));
+//     cb();
+// };
+//
+// exports.build = build;
+// exports.default = function(){
+//     gulp.watch('./source/scss/_partial/*.scss', build);
+//     gulp.watch('./source/scss/*.scss', build);
+// }
+//
+
 var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-function build(cb){
-    gulp.src('./source/scss/*.scss')
+// 一次性编译 Sass 
+gulp.task('sass', function() {
+    return gulp.src('./source/scss/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        // .pipe(sass({outputStyle: 'expanded'}))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
-    cb();
-};
+});
 
-exports.build = build;
-exports.default = function(){
-    gulp.watch('./source/scss/_partial/*.scss', build);
-    gulp.watch('./source/scss/*.scss', build);
-}
-
+// 实时编译
+gulp.task('default', ['sass'], function() {
+    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
+    gulp.watch('./source/scss/*.scss', ['sass']);
+});
